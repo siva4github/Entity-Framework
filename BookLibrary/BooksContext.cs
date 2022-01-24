f794b44d4bf98fe6631a3106f0bbbf39f804c3b5
Siva Sankar Alamalakala <leo.shiv@gmail.com>
2022-01-24 23:26:55 +0530

    Using Fluent API

--- BookLibrary/BooksContext.cs
+++ BookLibrary/BooksContext.cs
@@ -16,5 +16,25 @@ namespace BookLibrary
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var books = modelBuilder.Entity<Book>();
+
+            books.ToTable("Volumes");
+            books.Property(b => b.Title).IsRequired();
+            // books.HasKey(b => b.ISBN); if single, multiple follow below
+            books.HasKey(b => new { b.ISBN, b.Title });
+            books.Ignore(b => b.Price);
+            books.HasOne(b => b.Author).WithMany(a => a.Books).HasForeignKey("Id_Author").IsRequired(); // if we want customised id then mentione
+            books.Property(b => b.YearOfPublication).HasColumnName("Year");
+            books.Property(b => b.Title).HasColumnType("varchar(20)");
+            books.Property(b => b.Title).HasMaxLength(20);
+            books.Property(b => b.Price).HasColumnType("decimal(5, 2)");
+            books.Property(b => b.SomeDate).HasColumnType("date");
+
+        }
+
     }
 }
