84884b937b2c5e428187aba5d41a67457ad18630
Siva Sankar Alamalakala <leo.shiv@gmail.com>
2022-01-24 14:21:56 +0530

    Code First Generation

---
+++ BookLibrary/BooksContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions<BooksContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
